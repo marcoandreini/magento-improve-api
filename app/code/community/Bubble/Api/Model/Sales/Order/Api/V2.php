ff898b9e88fb4eb32aab1a4bbd04536c3839fbdc
Maria Branca <m.branca@ikom.it>
2018-03-06 17:27:30 +0100

    tolto log

--- app/code/community/Bubble/Api/Model/Sales/Order/Api/V2.php
+++ app/code/community/Bubble/Api/Model/Sales/Order/Api/V2.php
@@ -1,12 +1,11 @@
 <?php
 class Bubble_Api_Model_Sales_Order_Api_V2 extends Mage_Sales_Model_Order_Api
 {
-
     public function info($orderIncrementId)
     {
         $result = parent::info($orderIncrementId);
         $order = parent::_initOrder($orderIncrementId);
-
+        //Mage::log(print_r($order,true),null,'ikom.log');
         if ($order->getBaseGiftVoucherDiscount() > 0) {
             $result['base_gift_voucher_discount'] = $order->getBaseGiftVoucherDiscount();
         }
